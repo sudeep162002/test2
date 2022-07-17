d27b59d4b19b59a50aa78254bad16f605d30d390
Ivan Ganatra <ivan.ganatra@gmail.com>
2022-07-17 17:46:23 +0530

    Bug corrected

--- src/utils/formAsyncFunctions.js
+++ src/utils/formAsyncFunctions.js
@@ -132,7 +132,7 @@ export const getIndividualStatisticalData = (formData) => {
                 {
                   totalCorrect++;
                 }
-                else{
+                else if(option.isMarked){
                   totalIncorrect++;
                 }
           });
